0183ab180b8e9bf2e345025641a10438ffbc36f5
Wharton <BW069340@cerner.net>
2021-07-15 13:29:27 -0500

    Adding a tool I built to convert decimal IP addresses to binary

---
+++ decimaltobinary.py
@@ -0,0 +1,18 @@
+# This is a tool I am going to use to convert a decimal IP address to binary
+
+def decimal_to_binary(ipaddress):
+    # First, I am going to split each part of the decimal IP address
+    number_list = ipaddress.split(".")
+
+    # Next I am going to format each item in the list as a binary number
+    for number in number_list:
+        bin_number = format(int(number), '08b')
+
+        # Convert the binary number back to a string
+        str_number = str(bin_number)
+
+        # Return the binary number to the user as a string
+        print(str(str_number), end=".")
+        
+
+        
